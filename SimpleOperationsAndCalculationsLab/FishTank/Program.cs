bae6f727d548c0d5d15f86eed689f2177db6b7f4
Ivan Enchev <63471679+3nch3v@users.noreply.github.com>
2020-08-02 11:57:01 +0200

    exercises

---
+++ SimpleOperationsAndCalculationsLab/FishTank/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace FishTank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int l = int.Parse(Console.ReadLine());
+            int w = int.Parse(Console.ReadLine());
+            int h = int.Parse(Console.ReadLine());
+            double percent = double.Parse(Console.ReadLine());
+
+            double area = (l * w * h) * 0.001;
+            double nettoArea = area * (1 - (percent * 0.01));
+
+            Console.WriteLine($"{nettoArea:f3}");
+        }
+    }
+}
---
+++ SimpleOperationsAndCalculationsLab/PetShop/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace PetShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dogs = int.Parse(Console.ReadLine());
+
+            int anotherDogs = int.Parse(Console.ReadLine());
+
+            double costs = dogs * 2.50 + anotherDogs * 4;
+
+            Console.WriteLine($"{costs:F2}lv.");
+        }
+    }
+}
