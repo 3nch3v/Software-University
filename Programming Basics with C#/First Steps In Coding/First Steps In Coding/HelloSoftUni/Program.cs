b4f551738afc558cc27b655ab3e6925beee4a622
Ivan Enchev <63471679+3nch3v@users.noreply.github.com>
2020-08-08 08:46:53 +0200

    new

---
+++ Programming Basics with C#/First Steps In Coding/First Steps In Coding/HelloSoftUni/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloSoftUni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Hello SoftUni");
+            }
+        }
+    }
+}
