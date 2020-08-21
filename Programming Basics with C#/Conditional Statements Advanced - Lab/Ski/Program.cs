58dd685e3ecf8a3b0f16678f0bb5975e9af88a8e
Ivan Enchev <63471679+3nch3v@users.noreply.github.com>
2020-08-21 16:11:37 +0200

    conditional statements

---
+++ Programming Basics with C#/Conditional Statements Advanced - Lab/Ski/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+namespace Ski
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int days = int.Parse(Console.ReadLine());
+            string room = Console.ReadLine();
+            string feedback = Console.ReadLine();
+
+            int residence = days - 1;
+
+            if (residence < 10)
+            {
+                if (feedback == "positive")
+                {
+                    switch (room)
+                    {
+                        case "room for one person": Console.WriteLine($"{(18.00 * residence) * 1.25:f2}"); break;
+                        case "apartment": Console.WriteLine($"{((25.00 * residence) * 0.7) * 1.25:f2}"); break;
+                        case "president apartment": Console.WriteLine($"{((35.00 * residence) * 0.9) * 1.25:f2}"); break;
+                        default:
+                            break;
+                    }
+                }
+
+                else if (feedback == "negative")
+                {
+                    switch (room)
+                    {
+                        case "room for one person": Console.WriteLine($"{(18.00 * residence) * 0.9:f2}"); break;
+                        case "apartment": Console.WriteLine($"{((25.00 * residence) * 0.7) * 0.9:f2}"); break;
+                        case "president apartment": Console.WriteLine($"{((35.00 * residence) * 0.9) * 0.9:f2}"); break;
+                        default:
+                            break;
+                    }
+                }
+            }
+
+            else if (residence >= 10 && residence <= 15)
+            {
+                if (feedback == "positive")
+                {
+                    switch (room)
+                    {
+                        case "room for one person": Console.WriteLine($"{(18.00 * residence) * 1.25:f2}"); break;
+                        case "apartment": Console.WriteLine($"{((25.00 * residence) * 0.65) * 1.25:f2}"); break;
+                        case "president apartment": Console.WriteLine($"{((35.00 * residence) * 0.85) * 1.25:f2}"); break;
+                        default:
+                            break;
+                    }
+                }
+
+                else if (feedback == "negative")
+                {
+                    switch (room)
+                    {
+                        case "room for one person": Console.WriteLine($"{(18.00 * residence) * 0.9:f2}"); break;
+                        case "apartment": Console.WriteLine($"{((25.00 * residence) * 0.65) * 0.9:f2}"); break;
+                        case "president apartment": Console.WriteLine($"{((35.00 * residence) * 0.85) * 0.9:f2}"); break;
+                        default:
+                            break;
+                    }
+                }
+            }
+
+            else if (residence > 15)
+            {
+                if (feedback == "positive")
+                {
+                    switch (room)
+                    {
+                        case "room for one person": Console.WriteLine($"{(18.00 * residence) * 1.25:f2}"); break;
+                        case "apartment": Console.WriteLine($"{((25.00 * residence) * 0.5) * 1.25:f2}"); break;
+                        case "president apartment": Console.WriteLine($"{((35.00 * residence) * 0.8) * 1.25:f2}"); break;
+                        default:
+                            break;
+                    }
+                }
+
+                else if (feedback == "negative")
+                {
+                    switch (room)
+                    {
+                        case "room for one person": Console.WriteLine($"{(18.00 * residence) * 0.9:f2}"); break;
+                        case "apartment": Console.WriteLine($"{((25.00 * residence) * 0.5) * 0.9:f2}"); break;
+                        case "president apartment": Console.WriteLine($"{((35.00 * residence) * 0.8) * 0.9:f2}"); break;
+                        default:
+                            break;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Programming Basics with C#/Conditional Statements Advanced - Lab/SmallShop/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace SmallShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string product = Console.ReadLine();
+            string city = Console.ReadLine();
+            double quantity = double.Parse(Console.ReadLine());
+
+            if (city == "Sofia")
+            {
+                switch (product)
+                {
+                    case "coffee": Console.WriteLine(quantity * 0.5); break;
+                    case "water": Console.WriteLine(quantity * 0.8); break;
+                    case "beer": Console.WriteLine(quantity * 1.2); break;
+                    case "sweets": Console.WriteLine(quantity * 1.45); break;
+                    case "peanuts": Console.WriteLine(quantity * 1.6); break;
+                    default:
+                        break;
+                }
+            }
+
+            else if (city == "Plovdiv")
+            {
+                switch (product)
+                {
+                    case "coffee": Console.WriteLine(quantity * 0.4); break;
+                    case "water": Console.WriteLine(quantity * 0.7); break;
+                    case "beer": Console.WriteLine(quantity * 1.15); break;
+                    case "sweets": Console.WriteLine(quantity * 1.30); break;
+                    case "peanuts": Console.WriteLine(quantity * 1.50); break;
+                    default:
+                        break;
+                }
+            }
+
+            else if (city == "Varna")
+            {
+                switch (product)
+                {
+                    case "coffee": Console.WriteLine(quantity * 0.45); break;
+                    case "water": Console.WriteLine(quantity * 0.7); break;
+                    case "beer": Console.WriteLine(quantity * 1.1); break;
+                    case "sweets": Console.WriteLine(quantity * 1.35); break;
+                    case "peanuts": Console.WriteLine(quantity * 1.55); break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
