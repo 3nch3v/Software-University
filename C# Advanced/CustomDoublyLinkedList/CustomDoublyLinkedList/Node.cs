efbb36225867f4694ac01ef101a13f325efc29b7
Ivan Enchev <63471679+3nch3v@users.noreply.github.com>
2020-10-14 18:49:55 +0200

    DoublyLinkedList

---
+++ C# Advanced/CustomDoublyLinkedList/CustomDoublyLinkedList/Node.cs
@@ -0,0 +1,15 @@
+﻿
+namespace CustomDoublyLinkedList
+{
+    class Node
+    {
+        public Node(int value)
+        {
+            this.Value = value;
+        }
+
+        public int Value { get; set; }
+        public Node NextNode { get; set; }
+        public Node PreviousNode { get; set; }
+    }
+}
---
+++ C# Advanced/CustomDoublyLinkedList/CustomDoublyLinkedList/StartUp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CustomDoublyLinkedList
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            DoublyLinkedList list = new DoublyLinkedList();
+
+            list.AddFirst(44);
+            list.AddFirst(22);
+            list.AddFirst(111111);
+
+            list.AddLast(1);
+            list.AddLast(11);
+            list.AddLast(111);
+
+            int[] arr = list.ToArray();
+
+            list.RemoveFirst();
+            list.RemoveLast();
+
+            list.ForEach(n => Console.WriteLine(n));
+        }
+    }
+}
