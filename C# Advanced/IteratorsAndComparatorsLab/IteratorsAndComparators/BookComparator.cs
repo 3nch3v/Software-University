f9fe2d201320d282ba1897aecf3f28f481f7c55e
Ivan Enchev <63471679+3nch3v@users.noreply.github.com>
2020-10-19 19:42:27 +0200

    IteratorsAndComparators

---
+++ C# Advanced/IteratorsAndComparatorsLab/IteratorsAndComparators/BookComparator.cs
@@ -0,0 +1,22 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+
+namespace IteratorsAndComparators
+{
+    public class BookComparator : IComparer<Book>
+    {
+        public int Compare(Book x, Book y)
+        {
+            int compare = x.Title.CompareTo(y.Title);
+
+            if (compare == 0)
+            {
+                compare = y.Year.CompareTo(x.Year);
+
+            }
+
+            return compare;
+        }
+    }
+}
