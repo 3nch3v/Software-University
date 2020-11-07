4d3d83b330380baacedaf0121532dd6bb51a0fb8
Ivan Enchev <63471679+3nch3v@users.noreply.github.com>
2020-11-07 14:48:45 +0100

    update

---
+++ C# OOP/InterfacesAndAbstrationEXERCISE/ExplicitInterfaces/Citizen.cs
@@ -0,0 +1,35 @@
+﻿
+using ExplicitInterfaces.Contracts;
+
+namespace ExplicitInterfaces
+{
+    public class Citizen : IResident, IPerson
+    {
+        public Citizen(string name, string country, int age)
+        {
+            Name = name;
+            Country = country;
+            Age = age;
+        }
+
+        public string Name { get; private set; }
+
+        public int Age { get; private set; }
+
+        public string Country { get; private set; }
+
+        string GetName()
+        {
+            return Name;
+        }
+
+        string IPerson.GetName()
+        {
+            return Name;
+        }
+        string IResident.GetName()
+        {
+            return $"Mr/Ms/Mrs {Name}";
+        }
+    }
+}
---
+++ C# OOP/InterfacesAndAbstrationEXERCISE/ExplicitInterfaces/Core/Engine.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ExplicitInterfaces.Contracts;
+
+namespace ExplicitInterfaces.Core
+{
+    public class Engine
+    {
+        public void Run()
+        {
+            string input;
+
+            while ((input = Console.ReadLine()) != "End")
+            {
+                string[] tokens = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                string name = tokens[0];
+                string country = tokens[1];
+                int age = int.Parse(tokens[2]);
+
+                Citizen citizen = new Citizen(name, country, age);
+
+                IPerson justName = citizen;
+                Console.WriteLine(justName.GetName());
+
+                IResident residentName = citizen;
+                Console.WriteLine(residentName.GetName());
+            }
+        }
+    }
+}
