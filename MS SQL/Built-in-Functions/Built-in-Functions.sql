b6fef2262b417c00eb8c54e6156262c2e8df8e09
Ivan Enchev <63471679+3nch3v@users.noreply.github.com>
2021-01-22 18:28:57 +0100

    db

--- MS SQL/Built-in-Functions/Built-in-Functions.sql
+++ MS SQL/Built-in-Functions/Built-in-Functions.sql
@@ -155,4 +155,34 @@ SELECT [Name] AS Game,
 	FROM Games
 		ORDER BY [Name], Duration, [Part of the Day]
 
---Problem 18.	Orders Table
+--Problem 18. Orders Table
+
+USE Orders;
+
+SELECT ProductName,
+          OrderDate,
+          DATEADD(DAY, 3, OrderDate) AS [Pay Due],
+          DATEADD(MONTH, 1, OrderDate) AS [Deliver Due]
+       FROM Orders
+
+--Problem 19. People Table
+
+CREATE TABLE People
+(
+       Id INT PRIMARY KEY IDENTITY,
+       [Name] VARCHAR(50) NOT NULL,
+       Birthdate DATETIME2 NOT NULL
+)
+
+INSERT INTO People ([Name], Birthdate)
+       VALUES ('Victor', '2000-12-07 00:00:00.000'),
+                ('Steven', '1992-09-10 00:00:00.000'),
+                ('Stephen', '1910-09-19 00:00:00.000'),
+                ('John', '2010-01-06 00:00:00.000')
+
+SELECT [Name],
+          DATEDIFF(YEAR, Birthdate, GETDATE()) AS [Age in Years],
+          DATEDIFF(MONTH, Birthdate, GETDATE()) AS [Age in Months],
+          DATEDIFF(DAY, Birthdate, GETDATE()) AS [Age in Days],
+          DATEDIFF(MINUTE, Birthdate, GETDATE()) AS [Age in Minutes]
+       FROM People
