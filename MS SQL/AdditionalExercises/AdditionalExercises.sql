e1c8d21b01e460741b1e2f0824800aa7e0849fae
Ivan Enchev <63471679+3nch3v@users.noreply.github.com>
2021-01-31 10:20:46 +0100

    db

--- MS SQL/AdditionalExercises/AdditionalExercises.sql
+++ MS SQL/AdditionalExercises/AdditionalExercises.sql
@@ -1,6 +1,6 @@
 USE Diablo;
 
---Problem 1.	Number of Users for Email Provider
+--1. Number of Users for Email Provider
 
 SELECT [Email Provider], COUNT([Email Provider]) AS [Number Of Users]
 	FROM
@@ -13,5 +13,34 @@ SELECT [Email Provider], COUNT([Email Provider]) AS [Number Of Users]
 
 
 --Problem 2.	All User in Games
+
+SELECT g.[Name] AS [Game], 
+	   gt.[Name] AS [Game Type],
+	   u.Username,
+	   ug.[Level],
+	   ug.Cash,
+	   c.[Name] AS [Character]
+	FROM UsersGames ug
+	JOIN Users u ON ug.UserId = u.Id
+	JOIN Characters c ON ug.CharacterId = c.Id
+	JOIN Games g ON ug.GameId = g.Id
+	JOIN GameTypes gt ON g.GameTypeId = gt.Id
+		ORDER BY [Level] DESC, Username, Game
+
+
+--Problem 3.	Users in Games with Their Items
+
+SELECT u.Username,
+       g.[Name] AS Game, 
+       COUNT(ugi.ItemId) AS [Items Count], 
+	   SUM(i.Price) AS [Items Price]
+  FROM Games AS g
+	JOIN UsersGames ug ON ug.GameId = g.Id
+	JOIN UserGameItems ugi ON ugi.UserGameId = ug.Id
+    JOIN Items i ON i.Id = ugi.ItemId
+	JOIN Users u ON u.Id = ug.UserId
+		GROUP BY g.[Name], u.Username
+			HAVING COUNT(ugi.ItemId) >= 10
+		ORDER BY [Items Count] DESC, [Items Price] DESC, u.Username
 
 
