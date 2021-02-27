3542706da41ff185b1f766e029418598a0dd217b
Ivan Enchev <63471679+3nch3v@users.noreply.github.com>
2021-02-27 11:57:22 +0100

    EntityRelations

---
+++ Entity Framework Core/Entityrelations/StudentSystem/P01_StudentSystem/Data/StudentSystemContext.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P01_StudentSystem.Data.Models;
+
+namespace P01_StudentSystem.Data
+{
+    public class StudentSystemContext : DbContext
+    {
+        public StudentSystemContext()
+        {
+        }
+
+        public StudentSystemContext(DbContextOptions options)
+            :base(options)
+        {
+        }
+
+
+       public DbSet<Course> Courses { get; set; }
+       public DbSet<Homework> Homeworks { get; set; }
+       public DbSet<Resource> Resources { get; set; }
+       public DbSet<Student> Students { get; set; }
+       public DbSet<StudentCourse> StudentsCourses { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(Configuration.ConnectionString);
+            }
+
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<StudentCourse>()
+                .HasKey(k => new {k.StudentId, k.CourseId});
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
