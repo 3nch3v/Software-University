66551a8633f01307545cefea1e73165ac78449a0
Ivan Enchev <63471679+3nch3v@users.noreply.github.com>
2021-03-02 22:06:49 +0100

    MusicHub

---
+++ Entity Framework Core/LINQ/MusicHub/MusicHub/Data/Models/Album.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace MusicHub.Data.Models
+{
+    public class Album
+    {
+        public Album()
+        {
+            Songs = new HashSet<Song>();
+        }
+
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(40)]
+        public string Name { get; set; }
+
+        [Required]
+        public DateTime ReleaseDate { get; set; }
+
+        public decimal Price => Songs.Sum(p => p.Price); //•Price – calculated property(the sum of all song prices in the album)
+
+        public int? ProducerId { get; set; }
+        public Producer Producer { get; set; }
+
+        public ICollection<Song> Songs { get; set; }
+    }
+}
