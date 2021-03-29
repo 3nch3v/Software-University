7a3624f128cba48cec58882d73a483bea1f19890
Ivan Enchev <63471679+3nch3v@users.noreply.github.com>
2021-03-29 10:40:02 +0200

    ExportUserPurchasesByType

---
+++ Entity Framework Core/Exampreparation8August2020/VaporStore/DataProcessor/Dto/Export/UserPurchsesExportModel.cs
@@ -0,0 +1,58 @@
+﻿namespace VaporStore.DataProcessor.Dto.Export
+{
+    using System.Xml.Serialization;
+
+    [XmlType("User")]
+    public class UserPurchsesExportModel
+    {
+        [XmlAttribute("username")]
+        public string Username { get; set; }
+
+        [XmlArray("Purchases")]
+        public PurchaseExportModel[] Purchases { get; set; }
+
+        [XmlElement("TotalSpent")]
+        public decimal TotalSpent { get; set; }
+    }
+
+    [XmlType("Purchase")]
+    public class PurchaseExportModel
+    {
+        [XmlElement("Card")]
+        public string Card { get; set; }
+
+        [XmlElement("Cvc")]
+        public string Cvc { get; set; }
+
+        [XmlElement("Date")]
+        public string Date { get; set; }
+
+        [XmlElement("Game")]
+        public GameExportModel Game { get; set; }
+    }
+
+    [XmlType("Game")]
+    public class GameExportModel
+    {
+        [XmlAttribute("title")]
+        public string Title { get; set; }
+
+        [XmlElement("Genre")]
+        public string Genre { get; set; }
+
+        [XmlElement("Price")]
+        public decimal Price { get; set; }
+    }
+}
+
+/*
+  <Purchase>
+        <Card>7991 7779 5123 9211</Card>
+        <Cvc>340</Cvc>
+        <Date>2017-08-31 17:09</Date>
+        <Game title="Counter-Strike: Global Offensive">
+          <Genre>Action</Genre>
+          <Price>12.49</Price>
+        </Game>
+      </Purchase>
+*/
--- Entity Framework Core/Exampreparation8August2020/VaporStore/DataProcessor/Serializer.cs
+++ Entity Framework Core/Exampreparation8August2020/VaporStore/DataProcessor/Serializer.cs
@@ -2,10 +2,16 @@
 {
     using System;
     using System.Linq;
-    using Data;
+    using System.Globalization;
+    using System.IO;
+    using System.Text;
+    using System.Xml.Serialization;
     using Newtonsoft.Json;
+    using Data;
+    using Data.Models.Enums;
+    using Dto.Export;
 
-	public static class Serializer
+    public static class Serializer
 	{
 		public static string ExportGamesByGenres(VaporStoreDbContext context, string[] genreNames)
         {
@@ -41,8 +47,56 @@
         }
 
 		public static string ExportUserPurchasesByType(VaporStoreDbContext context, string storeType)
-		{
-			throw new NotImplementedException();
-		}
+        {
+            StringBuilder sb = new StringBuilder();
+
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(UserPurchsesExportModel[]), new XmlRootAttribute("Users"));
+            XmlSerializerNamespaces namespaces = new XmlSerializerNamespaces();
+            namespaces.Add(string.Empty, string.Empty);
+
+            using StringWriter stringWriter = new StringWriter(sb);
+
+            PurchaseType purchaseTypeEnum = Enum.Parse<PurchaseType>(storeType);
+
+            var users = context
+                .Users
+                .ToArray()
+                .Select(u => new UserPurchsesExportModel
+                {
+                    Username = u.Username,
+                    Purchases = context
+                        .Purchases
+                        .ToArray()
+                        .Where(p => p.Card.User.Username == u.Username && p.Type == purchaseTypeEnum)
+                        .OrderBy(p => p.Date)
+                        .Select(p => new PurchaseExportModel
+                        {
+                            Card = p.Card.Number,
+                            Cvc = p.Card.Cvc,
+                            Date = p.Date.ToString("yyyy-MM-dd HH:mm", CultureInfo.InvariantCulture),
+                            Game = new GameExportModel
+                            {
+                                Title = p.Game.Name,
+                                Genre = p.Game.Genre.Name,
+                                Price = p.Game.Price
+                            }
+                        })
+                        .ToArray(),
+
+                    TotalSpent = context.Purchases
+                        .ToArray()
+                        .Where(p => p.Card.User.Username == u.Username && p.Type == purchaseTypeEnum)
+                        .Sum(p => p.Game.Price)
+                })
+                .Where(u => u.Purchases.Length > 0)
+                .OrderByDescending(u => u.TotalSpent)
+                .ThenBy(u => u.Username)
+                .ToArray();
+
+            xmlSerializer.Serialize(stringWriter, users, namespaces);
+
+            return sb.ToString().TrimEnd();
+        }
 	}
 }
+
