7c1b05d60c7761a72d36532245d98f3b2fd72f8c
Ivan Enchev <63471679+3nch3v@users.noreply.github.com>
2021-07-08 16:04:50 +0200

    Implement BalancedParentheses

--- Data Structures Fundamentals/Linear-Data-Structures-Exercise/04.BalancedParentheses/BalancedParenthesesSolve.cs
+++ Data Structures Fundamentals/Linear-Data-Structures-Exercise/04.BalancedParentheses/BalancedParenthesesSolve.cs
@@ -2,12 +2,48 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Linq;
 
     public class BalancedParenthesesSolve : ISolvable
     {
+        private static char[] openParentheseTypes = new[] { '(', '[', '{' };
+        private static char[] closeParentheseTypes = new[] { ')', ']', '}' };
+
         public bool AreBalanced(string parentheses)
         {
-            throw new NotImplementedException();
+            Stack<char> openParentheses = new Stack<char>();
+           
+            for (int i = 0; i < parentheses.Length; i++)
+            {
+                char currParenthese = parentheses[i];
+
+                if (openParentheseTypes.Contains(currParenthese))
+                {
+                    openParentheses.Push(currParenthese);
+                }
+                else if (closeParentheseTypes.Contains(currParenthese))
+                {
+                    if (openParentheses.Count == 0)
+                    {
+                        return false;
+                    }
+
+                    var openParenthese = openParentheses.Peek();
+
+                    if ((openParenthese == '(' && currParenthese == ')') 
+                        || (openParenthese == '{' && currParenthese == '}')
+                        || (openParenthese == '[' && currParenthese == ']'))
+                    {
+                        openParentheses.Pop();
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return openParentheses.Count == 0;
         }
     }
 }
